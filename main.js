f8f12f6f9112d1cde6856b0c6920ded9e08ebf77
TechMasterApp <84641823+TechMasterApp@users.noreply.github.com>
2023-12-17 15:25:48 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,68 @@
-function preload() {
+var leftX = 0
+var leftY = 0
+var rightX = 0
+var rightY = 0
+var song = ""
+var isPlaying = 0
+var volume = 1
+var rate = 1
+var scoreLeft = 0
+var scoreRight = 0
 
+function preload() {
+    song = loadSound("music.mp3")
+    song = loadSound("music2.mp3")
 }
 
 function setup() {
-    canvas = createCanvas(500, 500)
-    canvas.position(575, 300)
+    canvas = createCanvas(650, 500)
+    canvas.position(525, 300)
+    video = createCapture(VIDEO)
+    video.hide()
+    poseNet = ml5.poseNet(video, modelLoaded)
+    poseNet.on("pose", acceptResult)
 }
 
 function draw() {
+    image(video, 0, 0, 650, 500)
+    fill("red")
+    circle(leftX, leftY, 30)
+    circle(rightX, rightY, 30)
+    if (scoreLeft > scoreRight) {
+        document.getElementById('song').innerHTML = "Song Name: Peter Pan"
+    } else if (scoreRight > scoreLeft) {
+        document.getElementById('song').innerHTML = "Song Name: Harry Potter"
+    }
+}
 
+function playSong() {
+    if (isPlaying == 0) {
+        song.play()
+        song.rate(rate)
+        song.setVolume(volume)
+        isPlaying = 1
+    } else {
+        song.pause()
+        isPlaying = 0
+    }
+}
+
+function modelLoaded() {
+
+}
+
+function acceptResult(result) {
+    if (result.length > 0) {
+        scoreLeft = result[0].pose.keypoints[9].score
+        scoreRight = result[0].pose.keypoints[10].score
+        if (scoreLeft > scoreRight) {
+            song = loadSound("music.mp3")
+        } else if (scoreRight > scoreLeft) {
+            song = loadSound("music2.mp3")
+        }
+        leftX = result[0].pose.leftWrist.x
+        leftY = result[0].pose.leftWrist.y
+        rightX = result[0].pose.rightWrist.x
+        rightY = result[0].pose.leftWrist.y
+    }
 }
